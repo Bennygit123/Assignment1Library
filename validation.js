0b069216cce06d96e2fe849942f9187aa94f52b5
Bennygit123 <119126336+Bennygit123@users.noreply.github.com>
2024-02-03 01:41:48 +0530

    commit

--- validation.js
+++ validation.js
@@ -1,11 +1,16 @@
 // Password validation
 function validatePassword() {
+    var password = pwd.value;
     var passwordRegexp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
-    if (passwordRegexp.test(pwd.value)) {
-        err2.innerText += " | Password is strong";
+
+    if (passwordRegexp.test(password)) {
+        err2.innerText = "Password is strong";
         err2.style.color = "green";
+    } else if (password.length >= 8) {
+        err2.innerText = "Password is medium";
+        err2.style.color = "orange";
     } else {
-        err2.innerText += " | Password is weak";
+        err2.innerText = "Password is weak";
         err2.style.color = "red";
     }
 }
@@ -14,4 +19,10 @@ function validatePassword() {
 function validate() {
     validateEmail();
     validatePassword();
+
+    // Check if both email and password are valid
+    if (err2.innerText.includes("valid") && err2.innerText.includes("strong")) {
+        // Redirect to Success.html
+        window.location.href = "success.html";
+    }
 }
